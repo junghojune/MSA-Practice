f1af92b0765ca60b6548b29a7cfccfa56b128c57
hosu <hojune0904@gmail.com>
2024-08-05 23:49:45 +0900

    feignClient, Ribbon 사용하여 클라이언트 사이드 로드밸런싱 구현하기

--- com.msa.eureka.client.order/src/main/java/com/msa/eureka/client/order/OrderApplication.java
+++ com.msa.eureka.client.order/src/main/java/com/msa/eureka/client/order/OrderApplication.java
@@ -3,9 +3,10 @@ package com.msa.eureka.client.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableFeignClients
 public class OrderApplication {
 
 	public static void main(String[] args) {
